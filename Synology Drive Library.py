806f65c0097f4159175c8a691c223777bf59fce7
Cameron <cjupp@planarmotor.com>
2022-10-12 18:06:39 -0700

    File path discovered, need to test downloading

--- Synology Drive Library.py
+++ Synology Drive Library.py
@@ -1,4 +1,10 @@
 from synology_drive_api.drive import SynologyDrive
+import json
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
 NAS_IP = '192.168.1.65'
 NAS_PORT= '5001'
@@ -6,21 +12,28 @@ NAS_PORT= '5001'
 NAS_USER = 'SharedAccount'
 NAS_PASS = 'pQS3s6BPPn7u'
 
+NAS_USER = 'cjupp'
+NAS_PASS = 'ZJcj4GBSF9Frr9KCmGxy'
+
 prod_sheet = 'Production_Record_Testbench.osheet'
-file_path = '/cjupp/Projects/Serial Number Tool'
+file_path = '/team-folders/cjupp/Projects/Serial Number Tool'
+
+
 # Use specified port
 with SynologyDrive(NAS_USER, NAS_PASS, NAS_IP, NAS_PORT) as synd:
     print(synd.get_teamfolder_info())
 
-    print()
-    print()
-    print()
-    print()
+    print(synd.list_folder('/team-folders/cjupp'))
 
-    bio = synd.download_file(f'/mydrive/{prod_sheet}')
+    print(synd.get_file_or_folder_info('701208765814317115'))
+
+    #print()
+    #print()
+
+    """bio = synd.download_file(f'/mydrive/{prod_sheet}')
     with open(prod_sheet, 'wb') as f:
         f.write(bio)
-    pass
+    pass"""
 
 
 # get single label info
